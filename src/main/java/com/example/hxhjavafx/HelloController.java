6bd634a6932665a2001b6b8949586dc7ca5ac411
winsher-dotcom <frencesherwin.triste@unc.edu.ph>
2025-11-29 00:15:25 +0800

    12:15

--- src/main/java/com/example/hxhjavafx/HelloController.java
+++ src/main/java/com/example/hxhjavafx/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.hxhjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void openEmitterPage(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("emitter.fxml"));
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
+
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.hxhjavafx {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires javafx.base;
+    requires javafx.graphics;
 
     opens com.example.hxhjavafx to javafx.fxml;
     exports com.example.hxhjavafx;
